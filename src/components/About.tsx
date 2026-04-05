d5757fc2e65745627152ff5c71d148cd57caa1d2
Imnishantc <imnishantc1701@gmail.com>
2026-04-05 16:00:04 +0530

    Initial commit - my portfolio

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Namaste 🙏 I am Nishant Chavan, a 3rd-year Electronics Engineering student passionate about AI & Machine Learning. I develop projects in Data Science, Generative AI, and RAG systems. Seeking AI/ML internship opportunities.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Career.tsx
@@ -0,0 +1,71 @@
+import "./styles/Career.css";
+
+const Career = () => {
+  return (
+    <div className="career-section section-container">
+      <div className="career-container">
+        <h2>
+          My career <span>&</span>
+          <br /> experience
+        </h2>
+        <div className="career-info">
+          <div className="career-timeline">
+            <div className="career-dot"></div>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Team Leader - Stock Audit Project ABB India Ltd.,Nashik</h4>
+                <h5></h5>
+              </div>
+              <h3>Jan 2024</h3>
+            </div>
+            <p>
+              Led a team to conduct inventory verification and data reconciliation during a stock audit at ABB,
+              gaining hands-on industry experience and strengthening my leadership and analytical skills
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4> Vice President - Training and Placement Students Club</h4>
+                <h5></h5>
+              </div>
+              <h3>present</h3>
+            </div>
+            <p>
+              As Vice President of the T&P Club, I organize technical workshops and conduct group discussions, mock interviews, and aptitude sessions,motivating peers to enhance their professional skills
+              strengthening my leadership, communication, and organizational skills.
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4></h4>
+                <h5> </h5>
+              </div>
+              <h3></h3>
+            </div>
+            <p>
+              
+            </p>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4></h4>
+                <h5></h5>
+              </div>
+              <h3></h3>
+            </div>
+            <p>
+              
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Career;
